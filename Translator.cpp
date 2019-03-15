38e502a93800c7171e68f36c811bd98fb0b8b511
Cristian Bottazzi <cristian1604@educ.ar>
2019-03-15 11:33:33 -0300

    Improvements on translator functions

--- Translator.cpp
+++ Translator.cpp
@@ -1,16 +1,26 @@
 #include <string>
 #include <wx/textctrl.h>
+#include <iostream>
 using namespace std;
 
 void translate_8025_to_8035(wxTextCtrl* elem) {
 	//elem->Show(false);
 	string aux = std::string(elem->GetValue() + '\n');
 	elem->SetValue("");
+	bool program_initiated = false;
+	bool comments_inserted = false;
+	wxString comments;
 	
 	while (!aux.empty()) {
 		int pos = aux.find_first_of('\n');
 		string line = aux.substr(0, pos);
 		aux = aux.substr(pos+1, aux.length());
+		if (!program_initiated && line[0] != '%') {
+			comments.Append(';' + line + '\n');
+			continue;
+		} else {
+			program_initiated = true;
+		}
 		
 		while (!line.empty()) {
 			if (line[0] == ' ') {
@@ -25,19 +35,31 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 			line = line.substr(pos, line.length());
 			switch (sentence[0]) {
 			case '%':
+				if (!comments_inserted) {
+					elem->AppendText(sentence + '\n');
+					elem->AppendText(comments);
+					comments_inserted = true;
+				}
 				break;
 			case 'A':
 				sentence[0] = 'Q';
 				break;
 			case '(':
-				sentence = ';' + sentence + line;
+				sentence = ';' + sentence;
+				break;
+			case 'N':
+				// blank lines (block separator)
+				if (line.length() <= 2) {
+					sentence = sentence + "  ;";
+				}
 				break;
 			case '#':
 				sentence = sentence + line;
 				line = "";
 				break;
 			}
-			elem->AppendText(sentence);
+			if (comments_inserted && sentence[0] != '%')
+				elem->AppendText(sentence);
 		}
 		elem->AppendText('\n');
 	}
