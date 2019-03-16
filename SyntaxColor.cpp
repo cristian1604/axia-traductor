b509eb5b7685f243394185297bd8f8ab5aa17dae
Cristian <cristian1604@educ.ar>
2019-03-16 02:08:22 -0300

    Totally reimplemented syntax highlight method

--- SyntaxColor.cpp
+++ SyntaxColor.cpp
@@ -1,58 +1,62 @@
 #include <wx/textctrl.h>
 #include <string>
 #include <iostream>
+#include <map>
+#include <algorithm>
 using namespace std;
 
-void syntax_color_8025(wxTextCtrl* elem) {
-	//elem->Show(false);
-	string aux = std::string(elem->GetValue() + '\n');
-	elem->SetValue("");
+void syntax_highlight(wxTextCtrl* elem, int standard) {
+	map<char, wxTextAttr> Style;
+	char comment_symbol;
 	
-	while (!aux.empty()) {
-		int pos = aux.find_first_of('\n');
-		string line = aux.substr(0, pos);
-		aux = aux.substr(pos+1, aux.length());
-		
-		while (!line.empty()) {
-			if (line[0] == ' ') {
-				elem->AppendText(' ');
-				line = line.substr(1, line.length());
-				continue;
-			}
-			pos = line.find_first_of(' ');
-			if (pos < 0)
-				pos = line.length();
-			string sentence = line.substr(0, pos);
-			line = line.substr(pos, line.length());
-			switch (sentence[0]) {
-			case '%':
-				elem->SetDefaultStyle(wxTextAttr(wxColour( 28, 255, 251)));
-				break;
-			case 'N':
-				elem->SetDefaultStyle(wxTextAttr(*wxGREEN));
-				break;
-			case '(':
-				sentence = sentence + line;
-				line = "";
-				elem->SetDefaultStyle(wxTextAttr(wxColour(68, 117, 71)));
-				break;
-			case '#':
-				sentence = sentence + line;
-				line = "";
-				elem->SetDefaultStyle(wxTextAttr(wxColour(224, 13, 13)));
-				break;
-			default:
-				elem->SetDefaultStyle(wxTextAttr(*wxYELLOW));
-			}
-			elem->AppendText(sentence);
-		}
-		elem->AppendText('\n');
+	Style['%'] = wxTextAttr(wxColour( 28, 255, 251));
+	Style['M'] = wxTextAttr(*wxWHITE);
+	Style['N'] = wxTextAttr(*wxGREEN);
+	if (standard == 8025) {
+		Style['T'] = wxTextAttr(*wxBLUE);
+		comment_symbol = '(';
 	}
-	//elem->Show(true);
-	elem->SetFocus();
+	if (standard == 8035) {
+		Style['D'] = wxTextAttr(*wxBLUE);
+		comment_symbol = ';';
+	}
+	map<char, wxTextAttr>::iterator itStyle = Style.begin();
+	
+	elem->Show(false);
+	string aux = std::string(elem->GetValue() + '\n');
+	elem->SetStyle(0, aux.length(), wxTextAttr(*wxYELLOW));
+	string::iterator itStr = aux.begin();
+	long cur_pos = 0;
+	long cur_pos_end = 1;
+	wxTextAttr attr;
+	while (itStyle != Style.end()) {
+		while (cur_pos <= aux.length()) {
+			cur_pos = aux.find_first_of(itStyle->first, cur_pos);
+			if (cur_pos < 0) break;
+			cur_pos_end = min(aux.find_first_of(' ', cur_pos), aux.find_first_of('\n', cur_pos));
+			elem->SetStyle(cur_pos, cur_pos_end, itStyle->second);
+			cur_pos = cur_pos_end;
+		}
+		cur_pos = 0;
+		cur_pos_end = 1;
+		itStr = aux.begin();
+		++itStyle;
+	}
+	
+	// Highlight comments
+	cur_pos = 0;
+	while (cur_pos <= aux.length()) {
+		cur_pos = aux.find_first_of(comment_symbol, cur_pos);
+		if (cur_pos < 0) break;
+		cur_pos_end = aux.find_first_of('\n', cur_pos);
+		elem->SetStyle(cur_pos, cur_pos_end, wxTextAttr(wxColour(68, 117, 71)));
+		cur_pos = cur_pos_end;
+	}
+	elem->Show(true);
 }
 
-void syntax_color_8035(wxTextCtrl* elem) {
+
+void syntax_color_8035_OLD(wxTextCtrl* elem) {
 	//elem->Show(false);
 	string aux = std::string(elem->GetValue() + '\n');
 	elem->SetValue("");
--- SyntaxColor.h
+++ SyntaxColor.h
@@ -1,7 +1,6 @@
 #ifndef SYNTAXCOLOR_H
 #define SYNTAXCOLOR_H
 
-void syntax_color_8025(wxTextCtrl* elem);
-void syntax_color_8035(wxTextCtrl* elem);
+void syntax_highlight(wxTextCtrl* elem, int standard);
 
 #endif
