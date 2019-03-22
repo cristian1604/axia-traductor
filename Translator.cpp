e180963e83afd36732d59294e92171c4801f3d41
Cristian Bottazzi <cristian1604@educ.ar>
2019-03-22 12:06:14 -0300

    Changes on translator to detect blocks (WIP)

--- Translator.cpp
+++ Translator.cpp
@@ -4,8 +4,9 @@
 #include <algorithm>
 using namespace std;
 
+string block_conversion(string end, string &code, string &replacement);
+
 void translate_8025_to_8035(wxTextCtrl* elem) {
-	//elem->Show(false);
 	string translated;
 	string aux = std::string(elem->GetValue() + '\n');
 	elem->SetValue("");
@@ -18,7 +19,7 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 	
 	
 	while (!aux.empty()) {
-		int pos = aux.find_first_of('\n');
+		int pos = aux.find('\n');
 		string line = aux.substr(0, pos);
 		aux = aux.substr(pos+1, aux.length());
 		if (!program_initiated && line[0] != '%') {
@@ -34,7 +35,7 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 				line = line.substr(1, line.length());
 				continue;
 			}
-			pos = line.find_first_of(' ');
+			pos = line.find(' ');
 			if (pos < 0)
 				pos = line.length();
 			string sentence = line.substr(0, pos);
@@ -62,7 +63,7 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 				break;
 			case 'T':
 				// Convert form Taa.bb to Dbb
-				x = sentence.find_first_of('.',0);
+				x = sentence.find('.');
 				sentence = 'D' + sentence.substr(x+1,sentence.length());
 				break;
 			case 'K':
@@ -70,7 +71,7 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 				if (prevSentence != "G04") {
 					break;
 				}
-				x = min(sentence.find_first_of(' ',0), sentence.find_first_of('\n',0));
+				x = min(sentence.find(' ',0), sentence.find('\n',0));
 				sAux = sentence.substr(1,x);
 				sAux.ToDouble(&db);
 				sAux.Clear();
@@ -79,13 +80,45 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 				break;
 			}
 			
-			// analyze assign commands on remaining line before insert
-			int x = line.find("P1=Z",0);
+			cout<<line<<endl;
+			
+			/** FIXED CONVERSIONS **/
+			// analyze FIXED assign commands on remaining line before insert
+			string str = "P1=Z";
+			int x = line.find(str,0);
 			if (x >= 0) {
-				line = line.replace(x,4,"(P100=PPOSZ)");
+				line = line.replace(x,str.length(),"(P100=PPOSZ)");
 				sentence.append(line);
 				line = "";
 			}
+			
+			// 'Epilogue' detector. Convert a block of code.
+//			str = "P1 = P1 F2 P2";
+//			x = line.find(str,0);
+//			if (x >= 0) {
+//				string rep = "(P100 = P100 - P102)\nM00 M05\nG29 JUMP\nM30";
+//				aux = block_conversion("M30", aux, rep);
+//				line = "";
+//			}
+			
+			// Sentence "GoTo"
+			str = "G29";
+			x = line.find(str,0);
+			if (x >= 0) {
+				line = line.replace(x,str.length(),"(GOTO");
+				sentence.append(line + ")");
+				line = "";
+			}
+			
+			// Delete Line
+			str = "P1 = F11 P10";
+			x = line.find(str,0);
+			if (x >= 0) {
+				line = line.replace(x,str.length(),";");
+				sentence.append(line);
+				line = "";
+			}
+			
 			
 			if (comments_inserted && sentence[0] != '%') {
 				translated.append(sentence);
@@ -98,7 +131,23 @@ void translate_8025_to_8035(wxTextCtrl* elem) {
 		translated += '\n';
 		
 	}
-	//elem->Show(true);
 	elem->SetValue(translated);
 	elem->SetFocus();
+}
+
+/**
+	This function converts a block of code. The block begins on the start of &code
+	Arguments:
+	end = Command to identify end of block (INCLUSIVE)
+	code = Source code
+	replacement = Block of code to raplce
+**/
+string block_conversion(string end, string &code, string &replacement) {
+	int l_end = code.find(end, 0);
+	l_end += end.length();
+	if (l_end < 0) {
+		return "";
+	}
+	
+	return code.replace(0, l_end, replacement);
 }
