d4f99c2d8f16eb09f515eb62a877436661abfbd4
Cristian Bottazzi <cristian1604@educ.ar>
2019-04-05 14:33:57 -0300

    Deletion of old and unused functions

--- SyntaxColor.cpp
+++ SyntaxColor.cpp
@@ -6,6 +6,9 @@
 #include "wxOptions.h"
 using namespace std;
 
+#define FAGOR_8025 8025
+#define FAGOR_8035 8035
+
 void syntax_highlight(wxTextCtrl* elem, int standard, s_Settings &s) {
 	elem->Enable(false);
 	elem->SetStyle(0, (elem->GetValue()).length(), wxTextAttr(*wxYELLOW,s.colour_textCtrl));
@@ -15,11 +18,11 @@ void syntax_highlight(wxTextCtrl* elem, int standard, s_Settings &s) {
 	Style['%'] = wxTextAttr(wxColour( 28, 255, 251));
 	Style['M'] = wxTextAttr(s.colour_command_m);
 	Style['N'] = wxTextAttr(s.colour_line_number);
-	if (standard == 8025) {
+	if (standard == FAGOR_8025) {
 		Style['T'] = wxTextAttr(s.colour_command_tool);
 		comment_symbol = '(';
 	}
-	if (standard == 8035) {
+	if (standard == FAGOR_8035) {
 		Style['D'] = wxTextAttr(s.colour_command_tool);
 		comment_symbol = ';';
 	}
@@ -56,65 +59,4 @@ void syntax_highlight(wxTextCtrl* elem, int standard, s_Settings &s) {
 		cur_pos = cur_pos_end;
 	}
 	elem->Enable(true);
-}
-
-
-void syntax_color_8035_OLD(wxTextCtrl* elem) {
-	//elem->Show(false);
-	string aux = std::string(elem->GetValue() + '\n');
-	elem->SetValue("");
-	
-	while (!aux.empty()) {
-		int pos = aux.find_first_of('\n');
-		string line = aux.substr(0, pos);
-		aux = aux.substr(pos+1, aux.length());
-		wxColour bg = wxColour( 0, 30, 60);
-		
-		while (!line.empty()) {
-			if (line[0] == ' ') {
-				elem->AppendText(' ');
-				line = line.substr(1, line.length());
-				continue;
-			}
-			pos = line.find_first_of(' ');
-			if (pos < 0)
-				pos = line.length();
-			string sentence = line.substr(0, pos);
-			line = line.substr(pos, line.length());
-			switch (sentence[0]) {
-			case '%':
-				elem->SetDefaultStyle(wxTextAttr(wxColour( 28, 255, 251), bg));
-				break;
-			case 'D':
-				elem->SetDefaultStyle(wxTextAttr(*wxRED, bg));
-				break;
-			case 'M':
-				if (sentence == "M04" || sentence == "M03") {
-					elem->SetDefaultStyle(wxTextAttr(*wxRED, bg));
-				} else {
-					elem->SetDefaultStyle(wxTextAttr(*wxYELLOW, bg));
-				}
-				break;
-			case 'N':
-				elem->SetDefaultStyle(wxTextAttr(*wxGREEN, bg));
-				break;
-			case ';':
-				sentence = sentence + line;
-				line = "";
-				elem->SetDefaultStyle(wxTextAttr(wxColour(68, 117, 71), bg));
-				break;
-			case '#':
-				sentence = sentence + line;
-				line = "";
-				elem->SetDefaultStyle(wxTextAttr(wxColour(224, 13, 13), bg));
-				break;
-			default:
-				elem->SetDefaultStyle(wxTextAttr(*wxYELLOW, bg));
-			}
-			elem->AppendText(sentence);
-		}
-		elem->AppendText('\n');
-	}
-	//elem->Show(true);
-	//elem->SetFocus();
 }
